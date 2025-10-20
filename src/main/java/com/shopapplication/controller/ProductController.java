cf9b7bb712099cff89bd3811dea785172df149f1
Getabalewkemaw <getabalewkemaw@gmail.com>
2025-10-20 20:40:06 +0300

    updates the user model to srore profile images

--- src/main/java/com/shopapplication/controller/ProductController.java
+++ src/main/java/com/shopapplication/controller/ProductController.java
@@ -5,7 +5,6 @@ import com.shopapplication.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
 
--- src/main/java/com/shopapplication/models/User.java
+++ src/main/java/com/shopapplication/models/User.java
@@ -39,6 +39,7 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role; // USER or ADMIN
 
+    @Column(columnDefinition = "TEXT")
     private String profileImage;
 
     @Builder.Default
